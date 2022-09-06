c398c115236900cc2a72de742ba673d91774f3c4
PBillingsby <philbillingsby@gmail.com>
2022-09-06 11:52:06 -0400

    add correct config for permaweb deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  trailingSlash: true,
+  assetPrefix: './',
   images: {
     domains: ['arweave.net'],
   },
